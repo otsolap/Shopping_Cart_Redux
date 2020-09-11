9dc0a8fbf14afbb960f23da3527299b10ab7d189
otsolap <59653348+otsolap@users.noreply.github.com>
2020-09-11 22:25:26 +0300

    redux version added to cart components.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import data from './data.json';
+// import data from './data.json';
 import Products from './Components/Products';
 import Filter from './Components/Filter';
 import Cart from './Components/Cart';
@@ -9,86 +9,7 @@ import { Provider } from 'react-redux';
 
 class App extends Component {
   // render on funktio, kuten tiedämme ():stä.
-  constructor() {
-    super();
-    this.state = {
-      products: data.products,
-      size: "",
-      sort: "",
-      cartItems: JSON.parse(localStorage.getItem('cartItems'))
-        ? JSON.parse(localStorage.getItem('cartItems'))
-        : [],
-    }
-  }
 
-  createOrder = (order) => {
-    alert('We need to save order for ' + order.name)
-  }
-
-  removeFromCart = (product) => {
-    const cartItems = this.state.cartItems.slice();
-    this.setState({
-      cartItems: cartItems.filter((x) => x.id !== product.id),
-    })
-
-  }
-
-  addToCart = (product) => {
-    // slice => kopioitu versio. Ei state mutaatiota. ,-)
-    const cartItems = this.state.cartItems.slice();
-    let alreadyInCart = false;
-    cartItems.forEach(item => {
-      if (item.id === product.id) {
-        item.count++;
-        alreadyInCart = true;
-      }
-    });
-    if (!alreadyInCart) {
-      cartItems.push({ ...product, count: 1 });
-    }
-    this.setState({ cartItems });
-    localStorage.setItem('cartItems', JSON.stringify(this.state.cartItems));
-  }
-  /* Ei tarvita Reduxin jälkeen
-    sortProducts = (event) => {
-      const sort = event.target.value;
-      // event.target.value kertoo millä perusteella asiakas haluaa nähdä tuotteet.
-      this.setState((state) => ({
-        sort: sort,
-        products: this.state.products.slice().sort((a, b) => (
-          sort === 'lowest' ?
-            ((a.price > b.price)
-              ? 1
-              : -1)
-            : sort === 'highest'
-              ? ((a.price < b.price)
-                ? 1
-                : -1)
-              // uusin eka. ID numerolla se selviää se uusin.
-              // eli B.id on loppupäässä.
-              : ((a.id < b.id)
-                ? 1
-                : -1)
-        )),
-      }))
-    } */
-
-
-  /* React-Reduxin jälkeen ei tarvita.
-  filterProducts = (event) => {
-    // event.target.value kertoo minkä koon asiakasvalitsi.
-    if (event.target.value === "") {
-      this.setState({ size: event.target.value, product: data.products });
-    } else {
-      this.setState({
-        size: event.target.value,
-        // =>0 - Onko valittu koko valikoimassa? Jos < 0, niin sitä ei Ole ja pitää kertoa asiakkaalle
-        // että sorry size not available!
-        products: data.products.filter((product) => product.availableSizes.indexOf(event.target.value) >= 0),
-      });
-    }
-  };
-*/
   render() {
     return (
       <Provider store={store}>
@@ -100,21 +21,16 @@ class App extends Component {
             <div className="content">
               <div className="main">
                 <Filter />
-                <Products addToCart={this.addToCart}
-                />
+                <Products />
               </div>
               <div className="sidebar">
-                <Cart
-                  cartItems={this.state.cartItems}
-                  removeFromCart={this.removeFromCart}
-                  createOrder={this.createOrder}
-                />
+                <Cart />
               </div>
             </div>
           </main>
           <footer>
             All right's reserved!
-        </footer>
+      </footer>
         </div>
       </Provider>
     );
